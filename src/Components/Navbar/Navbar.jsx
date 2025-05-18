43b13ec30206e8012e492e65bc812143cbd21c1e
Wuttiphong2000 <wuttiphong.santhalunai@gmail.com>
2025-05-18 20:41:57 +0700

    responsive/up-react-router-dom-v.7

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,30 @@
-import "./Navbar.css";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import "./Navbar.css";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <nav>
+    <nav className="navbar">
       <Link to="/" className="logo-header">
         <h3>Blog Application</h3>
       </Link>
-      <div className="link">
-        <Link to="/">หน้าแรก</Link>
-        <Link to="/blog">บทความทั้งหมด</Link>
-        <Link to="/about">เกี่ยวกับเรา</Link>
+
+      <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </div>
+
+      <div className={`link ${menuOpen ? "open" : ""}`}>
+        <Link to="/" onClick={() => setMenuOpen(false)}>
+          หน้าแรก
+        </Link>
+        <Link to="/blog" onClick={() => setMenuOpen(false)}>
+          บทความทั้งหมด
+        </Link>
+        <Link to="/about" onClick={() => setMenuOpen(false)}>
+          เกี่ยวกับเรา
+        </Link>
       </div>
     </nav>
   );
